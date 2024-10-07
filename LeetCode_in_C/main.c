4d7548ff3a343cafd708f35e60d575fecb055c13
Andrew Ferruolo <andrew.ferruoo@gmail.com>
2024-10-07 08:48:40 -0400

    Initial Commit

---
+++ LeetCode_in_C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LeetCode_in_C
+//
+//  Created by Andrew Ferruolo on 10/7/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
